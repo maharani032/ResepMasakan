3c880108ca2234ffd65de357d4eaf06ba8e37c8b
Maharani Nirmala Dewi <contact.maharani.d@gmail.com>
2021-09-15 11:10:20 +0700

    add navbar

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.set( 'view engine', 'ejs' );
 app.set( 'views', 'views' );
+app.use( express.static( "public" ) );
 
 
 
---
+++ controllers/test.js
@@ -0,0 +1,11 @@
+// exports.getIndex = ( req, res, next ) =>
+// {
+//     res.render( 404 // // nama file yg dipake di folder views
+//     ).render(
+//         '404',
+//         {
+//             pageTitle: 'Page Not Found', // judul halaman
+//             path: '/404' //httpsnya
+
+//         } );
+// }
